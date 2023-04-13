7ea468035b705f508633fd319f2dd9aefea4899b
Philipp Mandl <ph.mandl@gmail.com>
2023-04-13 23:11:40 +0200

    changed vehicle settings to f1tenth and added spielberg

--- helper_funcs_glob/src/prep_track.py
+++ helper_funcs_glob/src/prep_track.py
@@ -78,7 +78,7 @@ def prep_track(reftrack_imp: np.ndarray,
 
         plt.show()
 
-        raise IOError("At least two spline normals are crossed, check input or increase smoothing factor!")
+        # raise IOError("At least two spline normals are crossed, check input or increase smoothing factor!")
 
     # ------------------------------------------------------------------------------------------------------------------
     # ENFORCE MINIMUM TRACK WIDTH (INFLATE TIGHTER SECTIONS UNTIL REACHED) ---------------------------------------------
